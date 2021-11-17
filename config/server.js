5ad019b6311d19670e27ae48f1dca24f342cc912
ahmed mostafa <ahmed.moustafa9663@gmail.com>
2021-11-17 15:37:42 +0200

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2da75a23268f859fdccd8f21f64e7f89'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '89b488e6-2a0b-4fa1-8ee5-8dea166f5d40'
+};
